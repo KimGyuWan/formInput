8a6f1f75c33f195620716768f85084584ded6d26
KimGyuWan <y5g5776@naver.com>
2024-01-02 11:17:16 +0900

    폼태그 초안

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='form_member'>
+      <form action="#none">
+        <h2>User Login</h2>
+        <ul>
+          <li>
+            <label htmlFor="email">
+              Eamil
+            </label>
+            <input ref={emailRef} type="text" id='email' required />
+          </li>
+          <li>
+            <label htmlFor="password">
+              Password
+            </label>
+            <input ref={passwordRef} type="text" id='password' required />
+          </li>
+        </ul>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
